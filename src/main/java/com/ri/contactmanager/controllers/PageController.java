d5252f594ca45c03f5e8431f8dbf742ef7141c0c
AsadurRahmanRishan59 <asadur.rahman.rishan@g.bracu.ac.bd>
2025-01-18 22:36:19 +0600

    initial-project

---
+++ src/main/java/com/ri/contactmanager/controllers/PageController.java
@@ -0,0 +1,34 @@
+package com.ri.contactmanager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+@Controller
+public class PageController {
+
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        System.out.println("Home page handler");
+        model.addAttribute("Name", "Rishan");
+        return "home";
+    }
+    
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/services")
+    public String services() {
+        return "services";
+    }
+    
+    
+}
